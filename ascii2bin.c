b5bb508ba556fbef512f5e5e026a8413232a2b3e
coding-yost <77023930+coding-yost@users.noreply.github.com>
2021-03-04 18:39:01 -0800

    Experimenting with printf() and ascii values

--- ascii2bin.c
+++ ascii2bin.c
@@ -16,35 +16,49 @@
 #define byte unsigned char
 
 
+    /*
+    # Assignment
+        Your task is to develop a C program, called ascii2bin, that
+            * reads a string of 1's and 0's as ASCII digits, and 
+            * outputs the equivalent unsigned decimal number 
+    */
+
+   /*
+        Your program must
+            1. exercises the read() system call to read a single byte, at a time, from stdin
+            2. validate that the read byte is appropriate for conversion, e.g., it must be either an ASCII '0' or '1'
+            3. converts each byte into an integer value via a mathematical expression
+            4. uses the resulting integer as part of the calcuation to determine the final number
+            5. identifies the end of a input string by either end of file or by a new line
+                5a  End of file is detected when read() returns the value '0'
+                5b  A new line is identified in the ASCII table as either: newline, nl, LF, or \n'
+            6. prints this final unsigned number on stdout
+            7. returns a value of 0 upon success and 1 otherwise
+    */
+
 int main (int argc, char * argv[], char ** envp) {
 
-    int offset = 0;
+    int offset = 30;
     int number = 0;
-    int ascii_value;
-    int digit;
-
-
-/* reads a string of 1's and 0's as ASCII digits */
-/* Exercise the read() system call to read a single byte, at a time from stdin */
-    
-    int retval = read(0, &ascii_value, 1);
+    char ascii_value;
+    int retval = read(0, &ascii_value, 1);      /* 1. */
+    printf("%i\n",retval);
+    printf("ascii_value: %d\n", ascii_value);
+    printf("first\n");
     
     while (retval == 1){
-        digit = ascii_value - offset;
+     
+        int digit = (int)ascii_value - offset;
+        printf("digit: %c\t", digit);
         number = (number << 1) + digit;
+        printf("number: %i\n", number);  
         retval = read(0, &ascii_value, 1);
-    }
+        printf("ascii_value: %d\n", ascii_value);
+        printf("retval: %i\n", retval);
+        printf("loop\n");
 
+    }
+    
     printf("%u\n", number);
     return 0;
-
-/* validate that the read byte is appropriate for conversion */
-
-   /* if (ascii_value <= 1){
-        fprintf(stderr, "Input not in byte format.\n");
-    }
-    */
-/* outputs the equivalent unsigned decimal number */
-
-
 }
